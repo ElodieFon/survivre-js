6334d23968100813a524dc8bfd33da8f3f2af985
elodie <e.fontaine.1991@hotmail.fr>
2022-04-14 20:05:14 +0200

    ajout de la classe joueur

--- js/sketchv2.js
+++ js/sketchv2.js
@@ -26,15 +26,59 @@ class Obstacle
         if (this.positionObstacleY < 0 + this.rayon/2 ) this.directionY *= + this.directionY ;       
     }  
 }
+class Joueur
+{
+    constructor()
+    {
+        this.diametreCercle = 50,
+        this.rayonCercle = this.diametreCercle/2,
+        this.posX = largeurPlateau/2,
+        this.posY = hauteurPlateau/2,
+        this.maxPosX = largeurPlateau - this.rayonCercle,
+        this.maxPosY = hauteurPlateau - this.rayonCercle,
+        this.minPosX = this.rayonCercle,
+        this.minPosY = this.rayonCercle,
+        this.nombreDePas = 5
+    }
+    display()
+    {
+        circle(this.posX, this.posY, this.diametreCercle); 
+    }
+    bouger() 
+    {
+        if (keyIsDown(RIGHT_ARROW)) //fleche droite du clavier
+        {
+            this.posX = this.posX + this.nombreDePas; //bouger dans la direction demandé
+        }    
+        else if (keyIsDown(LEFT_ARROW)) //fleche gauche du clavier
+        {
+            this.posX = this.posX - this.nombreDePas;
+        }    
+        else if (keyIsDown(DOWN_ARROW)) //fleche bas du clavier
+        {
+            this.posY = this.posY + this.nombreDePas;
+        }
+    
+        else if (keyIsDown(UP_ARROW)) //fleche haut du clavier
+        {
+            this.posY = this.posY - this.nombreDePas;
+        }
+    }
+
+}
 
 let largeurPlateau = 640;
 let hauteurPlateau = 480;
+let obs ;
+let player ;
 
 function setup(){
     createCanvas(largeurPlateau, hauteurPlateau); 
     noStroke();
+    frameRate(30);
    
   obs = new Obstacle();
+  player = new Joueur();
 }
 
 function draw(){
@@ -43,6 +87,12 @@ function draw(){
     obs.display();
     obs.bouger();
     obs.testColision();
+
+    fill('white');
+    stroke('black')
+    strokeWeight(2);
+    player.display();
+    player.bouger();
 }
 
 // on définit un classe qui s'appelle "Obstacle"
